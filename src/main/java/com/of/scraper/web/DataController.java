2d0e5dcd141130b127145b94bf0f46845f0ca96f
Matti Räty <mattiraty@hotmail.com>
2024-05-09 14:09:32 +0300

    created basic structure for DataController

---
+++ src/main/java/com/of/scraper/web/DataController.java
@@ -0,0 +1,27 @@
+package com.of.scraper.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.of.scraper.entity.Data;
+import com.of.scraper.service.DataService;
+import com.of.scraper.service.ScraperService;
+
+import lombok.AllArgsConstructor;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class DataController {
+
+    DataService dataService;
+    ScraperService scraperService;
+
+    @RequestMapping("/scrapeAndSave")
+    public void scrapeAndSave() {
+        Data data = scraperService.scrapeData();    
+        dataService.saveData(data);
+    }
+    
+}
