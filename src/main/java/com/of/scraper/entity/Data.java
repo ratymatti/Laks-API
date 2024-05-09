2944c7c73b929cca247147de2895549ab16618a1
Matti Räty <mattiraty@hotmail.com>
2024-05-09 14:08:20 +0300

    created basic structure for entity Data

---
+++ src/main/java/com/of/scraper/entity/Data.java
@@ -0,0 +1,30 @@
+package com.of.scraper.entity;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Data {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+}
