e5bbc016f3195422beb3404fffb20e1a84bff375
Matti Räty <mattiraty@hotmail.com>
2024-05-10 17:22:55 +0300

    added comments

--- src/main/java/com/of/scraper/service/ScraperService.java
+++ src/main/java/com/of/scraper/service/ScraperService.java
@@ -27,20 +27,30 @@ public class ScraperService {
     DataService dataService;
 
     public List<Data> scrapeData() {
+        // Initialize a list to store the scraped data
         List<Data> dataList = new ArrayList<>();
+
+        // Initialize the WebDriver
         WebDriver driver = initializeWebDriver();
+
+        // Navigate to the target page
         navigateToPage(driver, "https://www.scanatura.no/fangstrapport/?type=0&lang=2");
 
+        // Initialize the select element for "ELV" and select the value "1714"
         Select select = initializeSelect(driver, "ELV");
         select.selectByValue("1714");
+
+        // Wait for the page to load
         sleep(5000);
 
         // Get the current year
         int currentYear = getCurrentYear();
 
+        // Initialize the select element for "AAR" and get its options
         Select yearSelect = initializeSelect(driver, "AAR");
         List<WebElement> yearOptions = yearSelect.getOptions();
 
+        // Iterate over the year options in reverse order
         for (int i = (yearOptions.size() - 1); i >= 0; i--) {
             // Re-fetch the year options
             yearSelect = initializeSelect(driver, "AAR");
@@ -49,44 +59,65 @@ public class ScraperService {
             // Get the year value from the option
             int yearValue = Integer.parseInt(yearOptions.get(i).getAttribute("value"));
 
+            // If the year value is less than or equal to the current year
             if (yearValue <= currentYear) {
+                // Select the year value
                 yearSelect.selectByValue(String.valueOf(yearValue));
 
+                // Click the "btn_detalj" button
                 clickButton(driver, "btn_detalj");
 
+                // Initialize the select element for "Week" and get its options
                 Select weekSelect = initializeSelect(driver, "Week");
                 List<WebElement> weekOptions = weekSelect.getOptions();
 
+                // Iterate over the week options starting from the third option
+                // First two option contains summary data that we are not interested in
                 for (int j = 2; j < weekOptions.size(); j++) {
                     // Re-find the select element and re-get the options
                     weekSelect = initializeSelect(driver, "Week");
                     weekOptions = weekSelect.getOptions();
 
+                    // Get the value of the week option
                     String value = weekOptions.get(j).getAttribute("value");
+
+                    // Select the week value
                     weekSelect.selectByValue(value);
+
+                    // Wait for the page to load
                     sleep(5000);
 
                     // Now the page should be loaded with the selected option
+                    // Find the "DataGrid3" table
                     WebElement table = driver.findElement(By.id("DataGrid3"));
 
+                    // Get all the rows in the table
                     List<WebElement> rows = table.findElements(By.tagName("tr"));
 
+                    // Iterate over the rows starting from the second row
+                    // Ignore first row since its the header
                     for (int k = 1; k < rows.size(); k++) {
+                        // Get the current row
                         WebElement row = rows.get(k);
 
+                        // Get all the cells in the row
                         List<WebElement> cells = row.findElements(By.tagName("td"));
 
+                        // Create a Data object from the cells
                         Data data = createData(cells);
 
+                        // Save the data and add it to the list
                         Data savedData = dataService.saveData(data);
                         dataList.add(savedData);
                     }
                 }
             }
         }
+
         // Close the browser
         driver.quit();
 
+        // Return the list of scraped data
         return dataList;
     }
 
@@ -130,6 +161,8 @@ public class ScraperService {
     private int getCurrentYear() {
         int currentYear = LocalDate.now().getYear();
         if (LocalDate.now().getMonthValue() < 11) {
+            // If the current month is before November, consider the previous year.
+            // Data at the webpage is usually for the previous year. 
             currentYear--;
         }
         return currentYear;
