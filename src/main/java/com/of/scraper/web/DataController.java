37bb2edc4a01734021be7e5c4ed4ae6beaffb1e5
Matti Räty <mattiraty@hotmail.com>
2024-05-10 17:00:06 +0300

    changed return type

--- src/main/java/com/of/scraper/web/DataController.java
+++ src/main/java/com/of/scraper/web/DataController.java
@@ -1,5 +1,7 @@
 package com.of.scraper.web;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,8 @@ public class DataController {
 
     @GetMapping("/scrapeAndSave")
     public void scrapeAndSave() {
-        Data data = scraperService.scrapeData();    
-        dataService.saveData(data);
+        List<Data> data = scraperService.scrapeData();    
+        
     }
     
 }
