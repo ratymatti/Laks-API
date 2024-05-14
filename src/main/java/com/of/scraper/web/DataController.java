155d80b3911630b811cb06f40d2dba49c8eb7506
Matti Räty <mattiraty@hotmail.com>
2024-05-14 16:27:16 +0300

    added method getAllByNameAndSpecies

--- src/main/java/com/of/scraper/web/DataController.java
+++ src/main/java/com/of/scraper/web/DataController.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.of.scraper.dto.AnglerDTO;
 import com.of.scraper.entity.Data;
 import com.of.scraper.service.DataService;
 import com.of.scraper.service.ScraperService;
@@ -28,30 +26,18 @@ public class DataController {
 
     @GetMapping("/scrapeAndSave")
     public void scrapeAndSave() {
-        List<Data> data = scraperService.scrapeData();
-
+        scraperService.scrapeData();
     }
 
     @GetMapping("/getByName/{name}")
     public ResponseEntity<List<Data>> getByName(@PathVariable String name) {
         List<Data> data = dataService.findByName(name);
-        double totalWeight = data.stream()
-            .filter(Data -> Data.getSpecies().equals("Laks"))
-            .mapToDouble(Data::getWeight)
-            .sum();
-        System.out.println("Total weight: " + totalWeight);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Data> addNew(@RequestBody Data data) {
-        Data newData = dataService.save(data);
-        return new ResponseEntity<>(newData, HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/deleteAll")
-    public ResponseEntity<Void> deleteAll() {
-        dataService.deleteAll();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping("/getAllByNameAndSpecies/{name}/{species}")
+    public ResponseEntity<AnglerDTO> getAllByNameAndSpecies(@PathVariable String name, @PathVariable String species) {
+        AnglerDTO angler = dataService.findByNameAndSpecies(name, species);
+        return new ResponseEntity<>(angler, HttpStatus.OK);
     }
 }
