f28abccdcdce37273a1304ac796db277c1a8b347
Matti Räty <mattiraty@hotmail.com>
2024-05-14 16:25:13 +0300

    created individual method and couple of queries for optimizing requests

--- src/main/java/com/of/scraper/repository/DataRepository.java
+++ src/main/java/com/of/scraper/repository/DataRepository.java
@@ -3,13 +3,21 @@ package com.of.scraper.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.of.scraper.entity.Data;
 import java.util.List;
 
 
 public interface DataRepository extends JpaRepository<Data, UUID>{
-    List<Data> findByName(String name);
     List<Data> findAll();
-    void deleteAllInBatch();
+    List<Data> findByName(String name);
+    List<Data> findByNameAndSpecies(String name, String species);
+
+    @Query("SELECT COUNT(d) FROM Data d WHERE d.name = :name AND d.species = :species")
+    int getCountByNameAndSpecies(@Param("name") String name, @Param("species") String species);
+
+    @Query("SELECT SUM(d.weight) FROM Data d WHERE d.name = :name AND d.species = :species")
+    Double getTotalWeightByNameAndSpecies(@Param("name") String name, @Param("species") String species);
 }
