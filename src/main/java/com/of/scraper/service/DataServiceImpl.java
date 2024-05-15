aef47cb489a35c97fc66cb7552e91e8c80bbde40
Matti Räty <mattiraty@hotmail.com>
2024-05-15 14:47:12 +0300

    removed unused import

--- src/main/java/com/of/scraper/service/DataServiceImpl.java
+++ src/main/java/com/of/scraper/service/DataServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
