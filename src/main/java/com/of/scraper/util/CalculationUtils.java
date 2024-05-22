b3955aa9f2f3249fa98d8c92c6f11be07eb65a04
Matti Räty <mattiraty@hotmail.com>
2024-05-22 17:28:38 +0300

    created util class for some of DataProcessingService methods

---
+++ src/main/java/com/of/scraper/util/CalculationUtils.java
@@ -0,0 +1,71 @@
+package com.of.scraper.util;
+
+import java.util.List;
+
+import com.of.scraper.dto.DayDTO;
+import com.of.scraper.dto.YearDTO;
+
+public class CalculationUtils {
+
+    /**
+     * Rounds the total and average weights of salmons and seatrouts in a YearDTO
+     * to one decimal place.
+     * 
+     * @param yearDTO The YearDTO to be rounded.
+     * @return The YearDTO with rounded values.
+     */
+
+    public static YearDTO roundYearDTOValues(YearDTO yearDTO) {
+        yearDTO.setSalmonTotalWeight(roundToTwoDecimals(yearDTO.getSalmonTotalWeight()));
+        yearDTO.setSalmonAverageWeight(roundToTwoDecimals(yearDTO.getSalmonAverageWeight()));
+        yearDTO.setSeatroutTotalWeight(roundToTwoDecimals(yearDTO.getSeatroutTotalWeight()));
+        yearDTO.setSeatroutAverageWeight(roundToTwoDecimals(yearDTO.getSeatroutAverageWeight()));
+        return yearDTO;
+    }
+
+    /**
+     * Rounds a double value to two decimal places.
+     * 
+     * @param value The value to be rounded.
+     * @return The rounded value as double.
+     */
+
+     public static double roundToTwoDecimals(double value) {
+        final int NUM_OF_DECIMALS = 2;
+        return Math.round(value * Math.pow(10, NUM_OF_DECIMALS)) / Math.pow(10, NUM_OF_DECIMALS);
+    }
+
+    /**
+     * Calculates the average weight of fish caught in a week.
+     * 
+     * @param count The total fish count for the week.
+     * @param totalWeight The total weight of fish caught in the week.
+     * @return The average weight of fish caught in the week as double.
+     */
+
+     public static double calculateAverageWeight(int count, double totalWeight) {
+        return (count > 0) ? totalWeight / count : 0.0;
+    }
+
+    /**
+     * Calculates the total weight of fish caught in a week.
+     * 
+     * @param weekData A list of DayDTOs from same week, each representing a day's fish data.
+     * @return The total weight of fish caught in the week as double.
+     */
+
+    public static double calculateTotalWeight(List<DayDTO> weekData) {
+        return weekData.stream().mapToDouble(DayDTO::getTotalWeight).sum();
+    }
+
+    /**
+     * Calculates the total fish count for a week.
+     * 
+     * @param weekData A list of DayDTOs from same week, each representing a day's fish data.
+     * @return The total fish count for the week as int.
+     */
+
+    public static int calculateCount(List<DayDTO> weekData) {
+        return weekData.stream().mapToInt(DayDTO::getFishCount).sum();
+    }
+}
