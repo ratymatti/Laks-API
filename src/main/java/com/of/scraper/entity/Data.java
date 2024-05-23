a36e2863ce5d453d9cf8e3a3bbdc869c73d2dd61
Matti Räty <mattiraty@hotmail.com>
2024-05-23 16:09:21 +0300

    small change to comment

--- src/main/java/com/of/scraper/entity/Data.java
+++ src/main/java/com/of/scraper/entity/Data.java
@@ -30,7 +30,7 @@ import lombok.Setter;
  * @weight: double - the weight of the fish caught.
  * @species: String - the species of the fish caught.
  * @gear: String - the gear used for fishing.
- * @zone: String - the zone where the fishing took place.
+ * @zone: String - the zone where the fish was caught.
  * @name: String - the name of the angler.
  * @localDate: LocalDate - the local date when the fishing took place.
  */
@@ -42,41 +42,41 @@ import lombok.Setter;
 @Table(name = "data")
 public class Data {
 
-    public Data(String species, double weight, LocalDate localDate) {
-        this.species = species;
-        this.weight = weight;
-        this.localDate = localDate;
-    }
-
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
         name = "UUID",
         strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    private UUID id;
-
-    @Column(name = "location")
-    private String location;
-
-    @Column(name = "date")
-    private String date;
-
-    @Column(name = "weight")
-    private double weight;
-
-    @Column(name = "species")
-    private String species;
-
-    @Column(name = "gear")
-    private String gear;
-
-    @Column(name = "zone")
-    private String zone;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "local_date")
-    private LocalDate localDate;
+        )
+        private UUID id;
+        
+        @Column(name = "location")
+        private String location;
+        
+        @Column(name = "date")
+        private String date;
+        
+        @Column(name = "weight")
+        private double weight;
+        
+        @Column(name = "species")
+        private String species;
+        
+        @Column(name = "gear")
+        private String gear;
+        
+        @Column(name = "zone")
+        private String zone;
+        
+        @Column(name = "name")
+        private String name;
+        
+        @Column(name = "local_date")
+        private LocalDate localDate;
+        
+        public Data(String species, double weight, LocalDate localDate) {
+            this.species = species;
+            this.weight = weight;
+            this.localDate = localDate;
+        }
 }
